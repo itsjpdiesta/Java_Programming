fce383752a22e276a5f026d426ef3762bf7cc648
itsjpdiesta <91946004+itsjpdiesta@users.noreply.github.com>
2021-10-05 17:24:51 +0800

    Update FavoriteSong.java

--- Chapter01/src/Favorite_Song/FavoriteSong.java
+++ Chapter01/src/Favorite_Song/FavoriteSong.java
@@ -3,8 +3,8 @@ package Favorite_Song;
  * Project:  Java Programming
  * Package: Favorite_Song
  * Filename:  FavoriteSong
- * Created By:  Kimberly Henry
- * On: 29-May-2014 At: 6:59 PM
+ * Created By:  John Paul C. Diesta & Kyle Rei Roberto
+ * On: 05-September-2021 At: 5:00 PM
  *
  * Write, compile, and test a class that displays at least four lines of your favorite song.
  * Save the class as FavoriteSong.
@@ -13,11 +13,79 @@ public class FavoriteSong
 	{
 		public static void main(String[] args)
 			{
-				System.out.println("Each day through my window I watch as she passes by");
-				System.out.println("I say to myself your such a lucky guy");
-				System.out.println("To have a girl like her is truly a dream come true.");
-				System.out.println("Out of all the fellas in the world, she belongs to me.");
-				System.out.println("It was just my imagination, running away with me");
-				System.out.println("It was just my imagination once again, running away with me.");
+			        System.out.println("Me plus you, I'ma tell you one time.");
+			        System.out.println("Me plus you, I'ma tell you one time.");
+			        System.out.println("Me plus you, I'ma tell you one time.");
+			
+			        System.out.println("One time.");
+			        System.out.println("One time.");
+			
+				System.out.println("When I met you girl my heart went knock knock knock.");
+				System.out.println("Now them butterflies in my stomach won't stop stop.");
+				System.out.println("And even though it's a struggle love is all we got.");
+				System.out.println("So we gonna keep keep climbin' till the mountain top.");
+			
+				System.out.println("Your world is my world.");
+				System.out.println("And my fight is your fight.");
+			        System.out.println("My breath is your breath.");
+			        System.out.println("And your heart is my heart.");
+			
+			        System.out.println("You're my one love.");
+			        System.out.println("My one heart.");
+			        System.out.println("My one life for sure.");
+			        System.out.println("Let me tell you one time girl I love, girl I love you.");
+			        System.out.println("I'ma tell you one time girl I love, girl I love you.");
+			        System.out.println("And I'ma be your one guy.");
+			        System.out.println("You'll be my number one girl always makin' time for you.");
+			        System.out.println("I'ma tell you one time girl I love, girl I love you.");
+			        System.out.println("I'ma tell you one time girl I love, girl I love you.");
+			
+			        System.out.println("You look so deep.");
+			        System.out.println("You know that it humbles me.");
+			        System.out.println("You're by my side and what troubles them don't trouble me.");
+			        System.out.println("Many have called but the chosen is you.");
+			        System.out.println("Whatever you want shawty I'll give it to you.");
+			
+			        System.out.println("Your world is my world.");
+			        System.out.println("And my fight is your fight.");
+			        System.out.println("My breath is your breath.");
+			        System.out.println("And your heart is my heart.");
+			
+			        System.out.println("You're my one love.");
+			        System.out.println("My one heart.");
+			        System.out.println("My one life for sure.");
+			        System.out.println("Let me tell you one time girl I love, girl I love you.");
+			        System.out.println("I'ma tell you one time girl I love, girl I love you.");
+			        System.out.println("And I'ma be your one guy.");
+			        System.out.println("You'll be my number one girl always makin' time for you.");
+			        System.out.println("I'ma tell you one time girl I love, girl I love you.");
+			        System.out.println("I'ma tell you one time girl I love, girl I love you.");
+			        
+			        System.out.println("Shawty right there.");
+			        System.out.println("She's got everything I need and I'ma tell her one time.");
+			        System.out.println("One time, one time.");
+			        System.out.println("Give you everything you need down to my last dime.");
+			        System.out.println("She makes me happy.");
+			        System.out.println("I know where I'll be.");
+			        System.out.println("Right by your side 'cause she is the one.");
+			        System.out.println("For me.");
+			
+			        System.out.println("You're my one love.");
+			        System.out.println("My one heart.");
+			        System.out.println("My one life for sure.");
+			        System.out.println("Let me tell you one time girl I love, girl I love you.");
+			        System.out.println("I'ma tell you one time girl I love, girl I love you.");
+			        System.out.println("And I'ma be your one guy.");
+			        System.out.println("You'll be my number one girl always makin' time for you.");
+			        System.out.println("I'ma tell you one time girl I love, girl I love you.");
+			        System.out.println("I'ma tell you one time girl I love, girl I love you.");
+			
+			        System.out.println("Me plus you, I'ma tell you one time.");
+			        System.out.println("Me plus you, I'ma tell you one time.");
+			        System.out.println("Me plus you, I'ma tell you one time.");
+			
+			        System.out.println("One time.");
+			        System.out.println("One time.");
+			
 			}
 	}
